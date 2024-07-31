1dfba21d6c3d94370da5a506e5a24797545fb470
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-07-31 22:27:49 +0530

    handling moving to next question

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 // import "./index.css";
 
@@ -36,6 +37,8 @@ function reducer(state, action) {
             ? state.points + currentQuestion.points
             : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       console.log("unknown action", action);
   }
@@ -65,11 +68,14 @@ function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
