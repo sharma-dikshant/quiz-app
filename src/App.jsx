c246a305954d79c4ef1b06fd187f433f86f322f8
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-07-31 17:14:07 +0530

    handled question loading using useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { useReducer } from "react";
 // import "./index.css";
-function App() {
 
+const initialState = {
+  question: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      console.log("unknown action", action);
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // console.log(state)
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("errer", err));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
